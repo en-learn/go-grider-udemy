cd4960dccc777c09ab1f9d95161d6fb613afa3b5
Elias Norrby <elias.norrby@gmail.com>
2020-07-23 01:28:20 +0200

    L14: Variable Declarations

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+  // var card string = "Ace of Spades"
+  // This line is equivalent to the previous.
+  // Go will infer the type.
+  card := "Ace of Spades"
+  // The following line would throw an error.
+  // := is only used when declaring a new variable.
+  // card := "Five of Diamonds"
+  card = "Five of Diamonds"
+
+  fmt.Println(card)
 }
