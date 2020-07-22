c3e60627c30b736b04449a1ffe88f31f11d9525b
Elias Norrby <elias.norrby@gmail.com>
2020-07-23 01:36:34 +0200

    L15: Functions and Return Types

--- cards/main.go
+++ cards/main.go
@@ -3,14 +3,11 @@ package main
 import "fmt"
 
 func main() {
-  // var card string = "Ace of Spades"
-  // This line is equivalent to the previous.
-  // Go will infer the type.
-  card := "Ace of Spades"
-  // The following line would throw an error.
-  // := is only used when declaring a new variable.
-  // card := "Five of Diamonds"
-  card = "Five of Diamonds"
+  card := newCard()
 
   fmt.Println(card)
+}
+
+func newCard() string {
+  return "Five of Diamonds"
 }
