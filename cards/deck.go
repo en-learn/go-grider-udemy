31cd8df7a28e7b0d697b04fc6607f2fb645f35e7
Elias Norrby <elias.norrby@gmail.com>
2020-07-24 21:43:26 +0200

    L33: Asserting Elements in a Slice

--- cards/deck.go
+++ cards/deck.go
@@ -16,7 +16,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Cloves"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
