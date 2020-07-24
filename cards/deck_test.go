554321e004a39aaa2e6b7316f8111a97580a2192
Elias Norrby <elias.norrby@gmail.com>
2020-07-24 20:34:02 +0200

    L32: Writing Useful Tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,3 +1,11 @@
 package main
 
+import "testing"
 
+func Test_newDeck(t *testing.T) {
+  d := newDeck()
+
+  if len(d) != 16 {
+    t.Errorf("Expected deck length of 16, but got %v", len(d))
+  }
+}
