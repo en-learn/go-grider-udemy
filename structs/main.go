40aad4ab3c0d88e124c9a02f1003ff862bb220be
Elias Norrby <elias.norrby@gmail.com>
2020-07-27 11:01:11 +0200

    L38: Declaring Structs

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,18 @@
 package main
 
+import "fmt"
+
 type person struct {
   firstName string
   lastName string
 }
 
 func main() {
+  // This is dependant on the order in which the fields
+  // are listed in the struct definition. Ugh!
+  // alex := person{"Alex", "Anderson"}
 
+  alex := person{firstName: "Alex", lastName: "Anderson"}
+
+  fmt.Println(alex)
 }
